3c60075317a33f959dd5e885bb669fe2852a87ad
260360445 <l>
2018-06-22 19:53:13 +0800

    2016

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '122.14.210.145',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'mudi',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => '799309d74c62ca915af7ea9df5e618c0',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
